216931068a7e54ef695d451b5c2d85f56774eed0
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2024-10-27 00:30:26 +0530

    feat(portfolio): Enhance project showcases with detailed descriptions
    Revamped the project showcase section to provide more comprehensive information about each project. This includes adding detailed descriptions outlining key features, technologies used, and the impact of each project. The update aims to offer visitors a deeper understanding of my skills and experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { FaXTwitter } from "react-icons/fa6";
 import { ArrowUpRight } from "lucide-react";
 import { useRef } from "react";
 import { navItems } from "./components/navbar";
-import Link from "next/link";
 
 const social_links = [
   {
@@ -47,47 +46,50 @@ const skills = [
 ];
 
 const projectData = [
-  {
-    id: 32412,
-    name: "Postly",
-    git: "https://github.com/yagyaraj234/collab",
-    status: "In Progress",
-    live: "https://collab-neon.vercel.app/",
-    about: [""],
-  },
+  // {
+  //   id: 32412,
+  //   name: "Postly",
+  //   git: "https://github.com/yagyaraj234/collab",
+  //   status: "⏳ In Progress",
+  //   live: "https://collab-neon.vercel.app/",
+  //   about: [""],
+  // },
   {
     id: 1242,
     name: "Workbot",
     git: "https://github.com/yagyaraj234/collab",
-    status: "Completed",
+    status: "⏳ In Progress",
     live: "https://workbot.site",
-    about: [""],
+    about: [
+      "Architected a versatile document processing system using RAG methodology, integrating Pinecone for vector embeddings and Jina AI for robust data extraction across multiple file formats (PDFs, code, plain text).",
+      "Developed an intuitive workspace system that allows users to create dedicated environments for different purposes (general chat, code reviews, YouTube video analysis), each optimized for specific content types and use cases.",
+      "Engineered persistent chat history and similarity search functionality, enabling contextual conversations and intelligent information retrieval across different file types and previous interactions.",
+    ],
   },
   {
     id: 12,
     name: "Collab",
     git: "https://github.com/yagyaraj234/collab",
-    status: "Completed",
+    status: "📦 Completed",
     live: "https://collab-neon.vercel.app/",
     about: [
-      "Collab is a platform for teams to collaborate.",
-      "It is a platform where you can create a team, add members, create projects, and collaborate on them.",
-      "Built with Next.js, TailwindCSS, Prisma, and Clerk.",
+      "Designed and implemented a Trello-like application architecture, allowing users to create organizations, multiple boards per organization, and manage tasks with status updates and due dates",
+      "Integrated Stripe for seamless payment processing within the app, enhancing user experience and enabling subscription management for premium features.",
     ],
   },
-  {
-    id: 2,
-    name: "CoinfolioX",
-    git: "https://github.com/yagyaraj234/coinfolioX",
-    // img: project2,
-    live: "https://coinfolioX.vercel.app",
-  },
-  {
-    id: 3,
-    name: "Git stats Checker",
-    git: "https://github.com/yagyaraj234/github-stats-checker",
-    live: "https://github-stats-checker.vercel.app/",
-  },
+  // {
+  //   id: 2,
+  //   name: "CoinfolioX",
+  //   git: "https://github.com/yagyaraj234/coinfolioX",
+  //   // img: project2,
+  //   live: "https://coinfolioX.vercel.app",
+  // },
+  // {
+  //   id: 3,
+  //   name: "Git stats Checker",
+  //   git: "https://github.com/yagyaraj234/github-stats-checker",
+  //   live: "https://github-stats-checker.vercel.app/",
+  // },
 ];
 export default function Home() {
   const about = useRef(null);
@@ -107,8 +109,7 @@ export default function Home() {
             const isActive = item.name === "about" ? true : false;
             return (
               <div
-                // href={item.link}
-                className={`p-2 transition-colors ease-in-out duration-700  text-gray-500 hover:text-gray-900 dark:hover:text-white ${
+                className={`p-2 transition-colors ease-in-out duration-700  text-gray-500 hover:text-gray-900 dark:hover:text-white cursor-pointer ${
                   isActive ? "dark:text-white  text-gray-900" : ""
                 }`}
                 onClick={() => scrollToSection(item.ref)}
@@ -132,7 +133,7 @@ export default function Home() {
         </div>
         {/* About */}
         <div className="flex flex-col gap-2 ">
-          <h1>I'm Yagyaraj</h1>
+          <h1 className="text-xl normal-case">Hey👋, I'm Yagyaraj</h1>
           <h2>A full-stack software engineer, from India.</h2>
 
           <div className="flex gap-x-2">
@@ -170,38 +171,43 @@ export default function Home() {
 
       <div className=" p-4 space-y-4 bg-neutral-100 rounded-md mt-6 dark:bg-zinc-800">
         <div>
-          {/* I'd be excited to discuss potential collaboration opportunities and
-          learn more about your needs. */}
-          {/* I'm excited to continue learning and growing in this field, and I'm */}
-          {/* also open */}
-          {/* always looking for new opportunities */}
           I'm open to collaborate with talented individuals and contribute to
           impactful projects. If you'd like to learn more about my work or
           discuss potential opportunities, feel free to reach out!
         </div>
 
         <div className="flex gap-4 items-center">
-          <button className="bg-black rounded-md p-3 text-white text-sm hover:bg-black/80  transition-colors duration-300 ease-in-out">
+          <button
+            className="bg-black rounded-md p-3 text-white text-sm hover:bg-black/80  transition-colors duration-300 ease-in-out"
+            onClick={() => window.open("https://x.com/yagyaraj234")}
+          >
             Drop message on X
           </button>
 
-          <button className="group">
-            <a
-              href=""
-              className="flex justify-between items-center text-sm dark:text-white "
-            >
+          <button
+            className="group"
+            onClick={() =>
+              window.open(
+                "https://drive.google.com/file/d/1HH94EgoL2M73UtJD4miG74NAdxWg-HGS/view?usp=sharing"
+              )
+            }
+          >
+            <div className="flex justify-between items-center text-sm dark:text-white">
               Resume
               <ArrowUpRight
                 className=" group-hover:translate-x-1 transition-all duration-300 ease-in-out"
                 size={16}
               />
-            </a>
+            </div>
           </button>
         </div>
       </div>
 
-      <div className="mt-8" ref={journeyRef}>
-        <h2 className="font-semibold"> journey </h2>
+      <div className="mt-8">
+        <h2 className="font-semibold" ref={journeyRef}>
+          {" "}
+          journey.{" "}
+        </h2>
 
         <div className="flex flex-col gap-[16px] w-full space-y-4 mt-4">
           <div className="group flex gap-4">
@@ -289,8 +295,11 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="mt-8" ref={projectsRef}>
-        <h2 className="font-semibold"> projects </h2>
+      <div className="mt-8">
+        <h2 className="font-semibold" ref={projectsRef}>
+          {" "}
+          projects{" "}
+        </h2>
 
         <div className="flex flex-col gap-[16px] w-full space-y-4 mt-4">
           {projectData.map((project, idx) => (
@@ -300,7 +309,12 @@ export default function Home() {
               <div className="w-full">
                 <div className="flex justify-start min-w-full text-sm">
                   <div>
-                    <div className="text-lg  normal-case ">{project.name}</div>
+                    <div className="text-lg  normal-case flex gap-4 items-center ">
+                      {project.name}{" "}
+                      <span className="text-xs uppercase px-2 dark:bg-zinc-700 hover:bg-zinc-800 rounded-[4px] py-1 my-1 transition-colors duration-500 ease-in-out delay-75">
+                        {project.status}
+                      </span>
+                    </div>
                     <div className="flex gap-4">
                       <div className="flex gap-1 items-center group/live  ">
                         <a href={project.live} target="_black">
