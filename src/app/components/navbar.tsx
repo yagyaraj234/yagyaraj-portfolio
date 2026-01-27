5e3b130a4f4ede75611bae07e60aec2a5df848b4
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-01-28 01:52:08 +0530

    fix mobile responsiveness

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -27,14 +27,17 @@ export const navItems: NavItem[] = [
 
 export const Navbar = () => {
   return (
-    <header className="flex justify-end items-center pt-4">
-      <nav className="flex lg:gap-2" role="navigation">
+    <header className="flex justify-end items-center pt-4 w-full">
+      <nav
+        className="flex lg:gap-2 gap-1 items-center overflow-x-auto"
+        role="navigation"
+      >
         {navItems.map((item: NavItem, idx: number) => {
           return (
             <Link
               key={idx}
               href={item.link}
-              className={`sm:p-2 transition-colors ease-in-out duration-700 dark:text-gray-400 text-gray-500 hover:text-gray-900 dark:hover:text-white cursor-pointer  max-sm:hidden`}
+              className={`px-2 py-1 transition-colors ease-in-out duration-700 dark:text-gray-400 text-gray-500 hover:text-gray-900 dark:hover:text-white cursor-pointer whitespace-nowrap text-sm sm:text-base max-sm:hidden`}
             >
               {item.name}
             </Link>
