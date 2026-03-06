d26ffba4e0a1c45c3e3c4d733b5568b057b5307b
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-03-06 23:07:29 +0530

    feat: optimize performance with dynamic imports and refine UI layout
    - Implement dynamic loading for the GitHubContributions component to improve initial page load.
    - Add `next-sitemap`, `prettier`, and `tailwind-scrollbar` dependencies.
    - Refine layout in `Journey` and `Navbar` components for better spacing and consistency.
    - Configure `tailwind-scrollbar` plugin in `globals.css`.
    - Update `RootLayout` to set the default theme to "light" and adjust body scrollbar behavior.
    - Change `GitHubContributions` to a default export for easier dynamic importing.

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -28,22 +28,19 @@ export const navItems: NavItem[] = [
 export const Navbar = () => {
   return (
     <header className="flex justify-end items-center pt-4 w-full">
-      <nav
-        className="flex lg:gap-2 gap-1 items-center overflow-x-auto"
-        role="navigation"
-      >
+      <nav className="flex lg:gap-6 gap-4 items-center" role="navigation">
         {navItems.map((item: NavItem, idx: number) => {
           return (
             <Link
               key={idx}
               href={item.link}
-              className={`px-2 py-1 transition-colors ease-in-out duration-700 dark:text-gray-400 text-gray-500 hover:text-gray-900 dark:hover:text-white cursor-pointer whitespace-nowrap text-sm sm:text-base max-sm:hidden`}
+              className={`transition-colors ease-in-out duration-700 dark:text-gray-400 text-gray-500 hover:text-gray-900 dark:hover:text-white cursor-pointer whitespace-nowrap text-sm sm:text-base max-sm:hidden`}
             >
               {item.name}
             </Link>
           );
         })}
-        <ThemeButton />
+        {/* <ThemeButton /> */}
       </nav>
     </header>
   );
