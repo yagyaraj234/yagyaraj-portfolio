674418428e006b0fa024abb98bcb280f36693e53
Yagyaraj Lodhi <workyagyaraj@gmail.com>
2026-04-26 18:19:40 +0530

    chore: update resume PDF link to latest version

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,6 +2,6 @@ import { redirect } from "next/navigation"
 
 export default function ResumePage() {
   redirect(
-    `https://drive.google.com/file/d/1HDRjs-a3BH-40CQi_Ozr_aS175bhOzEm/view?usp=sharing`
+    `https://drive.google.com/file/d/1nVlptcsFdies5Y78ze6h6q4YiieRWUwj/view?usp=sharing`
   )
 }
